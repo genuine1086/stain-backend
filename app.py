7db094962831c58ecf2181373a5b9f9ee53abbb0
genuine1086 <aaronjw719@gmail.com>
2025-11-26 15:17:03 +0900

    add cors support

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 from ultralytics import YOLO
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 # Load model
 model = YOLO("best.pt")
